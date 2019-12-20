85a2088321f751c51097ca40706c6a6232c88e89
Marc Schier <marcschier@hotmail.com>
2019-12-20 16:03:48 +0100

    revert

--- common/src/Microsoft.Azure.IIoT.Hub.Module.Deployment/src/Properties/AssemblyInfo.cs
+++ common/src/Microsoft.Azure.IIoT.Hub.Module.Deployment/src/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Microsoft.Azure.IIoT.Hub.Edgemanager.Tests")]
+[assembly: InternalsVisibleTo("Microsoft.Azure.IIoT.Hub.Module.Deployment.Tests")]
